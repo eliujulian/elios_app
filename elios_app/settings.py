fc087c8cf13311f5a2373bdddd0ce5f99b6dd5cf
elius <mail@eliu.de>
2021-01-17 18:24:04 +0100

    changed secrets

--- elios_app/settings.py
+++ elios_app/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# for k, v in os.environ.items():
-#     print(k, v)
 SECRET_KEY = os.environ['SECRET_KEY_ELIOSAPP']
 
 # SECURITY WARNING: don't run with debug turned on in production!
