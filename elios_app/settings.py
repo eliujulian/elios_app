6de0546c869e2cab308486e73635e4a15ee7b40f
elius <mail@eliu.de>
2021-01-17 13:28:17 +0100

    changed secrets

--- elios_app/settings.py
+++ elios_app/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-from elios_app import secrets_git_exclude
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets_git_exclude.SECRET_KEY
+# for k, v in os.environ.items():
+#     print(k, v)
+SECRET_KEY = os.environ['SECRET_KEY_ELIOSAPP']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = secrets_git_exclude.ALLOWED_HOSTS
+ALLOWED_HOSTS = ['127.0.0.1', 'elios-app.heroku.com']
 
 
 # Application definition
