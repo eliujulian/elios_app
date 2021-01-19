d8ff044b6e833fceb113521f1516f8d667318b56
elius <mail@eliu.de>
2021-01-19 10:49:45 +0100

    User Model -> added __str__

--- core/models.py
+++ core/models.py
@@ -3,5 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        ordering = ['id']
 
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return self.username
