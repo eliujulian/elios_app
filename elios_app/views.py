2dabbf8b2669d5de5721d9b6872c55389489ae57
elius <mail@eliu.de>
2021-01-19 13:49:49 +0100

    removed home_test.html, replaced with base.html

--- elios_app/views.py
+++ elios_app/views.py
@@ -4,4 +4,4 @@ from django.template.response import TemplateResponse
 def test_view(request):
     context = {}
     context['data'] = None
-    return TemplateResponse(request, 'application/home_test.html', context)
+    return TemplateResponse(request, 'application/base.html', context)
