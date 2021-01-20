62516281654021ef8c84d6c89b22376bc3c728a4
elius <mail@eliu.de>
2021-01-20 08:30:43 +0100

    added definitions.py -> with Spheres definition

---
+++ core/definitions.py
@@ -0,0 +1,32 @@
+"""
+Some Static Lists that are relevant for different Apps.
+"""
+
+SPHERE_OF_LIFE_DE = (
+    (1, "Arbeit"),  # Work
+    (2, "Finanzen"),  # Money
+    (3, "Gesundheit"),  # Health
+    (4, "Freizeit"),  # Hobbies, Sport
+    (5, "Beziehungen"),  # Love
+    (6, "Familie"),  # Parents, Kids
+    (7, "Geselligkeit"),  # Friends
+    (8, "Persönlichkeit"),  # Yourself
+)
+
+SPHERE_OF_LIFE_EN = {
+    1: "Work",
+    2: "Finances",
+    3: "Health",
+    4: "Leisure",
+    5: "Love",
+    6: "Family",
+    7: "Friends",
+    8: "Personality"
+}
+
+PRIVACY_OPTIONS = (
+    (1, "Nur ich"),  #  Only user
+    (2, "Meine Kontakte"),   # User and contacts
+    (3, "Jeder"),  # Every user
+    (4, "Öffentlich")  # Every user and every anonymous user
+)
