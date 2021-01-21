9a7325e3e3e8878a31d2c19a517da69db44732dd
elius <mail@eliu.de>
2021-01-21 08:20:13 +0100

    tests for views account and user

--- tests/core/tests.py
+++ tests/core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.template.response import TemplateResponse
 from django.shortcuts import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from core.models import User
 
 
@@ -8,6 +9,7 @@ class CreateUserMixin:
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user("adam", "adam@adam.de", "123456")
+        self.user2 = User.objects.create_user("bdam", "adam@adam.de", "123456")
         self.client.login(username="adam", password="123456")
 
 
@@ -16,3 +18,45 @@ class LandingpageViewTest(CreateUserMixin, TestCase):
         response = self.client.get(reverse("landingpage"))
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response, TemplateResponse)
+
+    def test_get_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse("landingpage"))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+
+class UserDetaiViewTest(CreateUserMixin, TestCase):
+    def test_get_response_status_same_user(self):
+        response = self.client.get(reverse("user-detail", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+
+    def test_get_response_status_other_user(self):
+        response = self.client.get(reverse("user-detail", kwargs={"slug": "bdam"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+
+    def test_get_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse("user-detail", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+
+class AccountDetailViewTest(CreateUserMixin, TestCase):
+    def test_get_response_status_same_user(self):
+        response = self.client.get(reverse("account-detail", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+
+    def test_get_response_status_other_user(self):
+        response = self.client.get(reverse("account-detail", kwargs={"slug": "bdam"}))
+        self.assertEqual(response.status_code, 401)
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_get_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse("account-detail", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
