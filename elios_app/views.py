b5a6e367478674d8d847cf7af6bf6d81fb384d71
elius <mail@eliu.de>
2021-01-22 08:25:23 +0100

    landingview -> blocked post method

--- elios_app/views.py
+++ elios_app/views.py
@@ -1,7 +1,11 @@
 from django.template.response import TemplateResponse
-
+from django.http import HttpResponseNotAllowed
 
 def test_view(request):
     context = dict()
     context['data'] = None
+
+    if request.method == "POST":
+        return HttpResponseNotAllowed("get")
+
     return TemplateResponse(request, 'application/base.html', context)
