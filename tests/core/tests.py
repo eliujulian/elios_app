49e953dc9d1893cf3ae2aa09de39a15be517bd85
elius <mail@eliu.de>
2021-01-22 16:19:42 +0100

    more tests

--- tests/core/tests.py
+++ tests/core/tests.py
@@ -3,6 +3,7 @@ from django.template.response import TemplateResponse
 from django.shortcuts import reverse
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotAllowed
 from tests.texts_mixins import *  # noqa
+from core.views import *
 
 
 class UserDetaiViewTest(CreateUserMixin, TestCase):
@@ -68,3 +69,63 @@ class AccountUpdateViewTest(CreateUserMixin, TestCase):
         response = self.client.get(reverse("account-update", kwargs={"slug": "adam"}))
         self.assertEqual(response.status_code, 302)
         self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("login", response.url)
+
+    def test_post_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.post(reverse("account-update", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("login", response.url)
+
+    def test_post_response_other_user(self):
+        response = self.client.post(reverse("account-update", kwargs={"slug": "bdam"}))
+        self.assertEqual(response.status_code, 401)
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_post_response_same_user(self):
+        response = self.client.post(reverse("account-update", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("adam", response.url)
+
+    def test_post_response_same_user_change_first_name(self):
+        self.assertEqual(User.objects.get(username="adam").first_name, "")
+        response = self.client.post(reverse("account-update", kwargs={"slug": "adam"}), data={'first_name':"Adam"})
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("adam", response.url)
+        self.assertEqual(User.objects.get(username="adam").first_name, "Adam")
+
+
+class AccountDeleteViewTest(CreateUserMixin, TestCase):
+    def test_get_response_status_same_user(self):
+        response = self.client.get(reverse("account-delete", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+        self.assertIsInstance(response.context_data['view'], AccountDeleteView)
+
+    def test_get_response_status_other_user(self):
+        response = self.client.get(reverse("account-delete", kwargs={"slug": "bdam"}))
+        self.assertEqual(response.status_code, 401)
+        self.assertIsInstance(response, HttpResponse)
+
+
+    def test_get_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse("account-delete", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("login", response.url)
+
+    def test_post_response_other_user(self):
+        response = self.client.post(reverse("account-delete", kwargs={"slug": "bdam"}))
+        self.assertEqual(response.status_code, 401)
+        self.assertIsInstance(response, HttpResponse)
+
+    def test_post_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.post(reverse("account-delete", kwargs={"slug": "adam"}))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+        self.assertIn("login", response.url)
