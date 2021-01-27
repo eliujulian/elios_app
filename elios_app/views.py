8060f7a561f21f8799ded828a954bbe568a3c108
elius <mail@eliu.de>
2021-01-27 08:30:06 +0100

    landingpage_view -> only GET method allowed

--- elios_app/views.py
+++ elios_app/views.py
@@ -6,7 +6,7 @@ def test_view(request):
     context = dict()
     context['data'] = f"You are logged in as {request.user} ({request.user.username})."
 
-    if request.method == "POST":
+    if request.method != "GET":
         return HttpResponseNotAllowed("get")
 
     return TemplateResponse(request, 'application/base.html', context)
