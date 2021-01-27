81ace346aeda107631bba98299e1f7f09cef897d
elius <mail@eliu.de>
2021-01-27 09:54:52 +0100

    refactoring

--- elios_app/settings.py
+++ elios_app/settings.py
@@ -63,7 +63,6 @@ ROOT_URLCONF = 'elios_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': os.path.join(BASE_DIR, 'templates'),
         'DIRS': [BASE_DIR + '/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
