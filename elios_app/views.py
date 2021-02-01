192d9e690b8437f70df4e498c6c2c2502952f8a7
elius <mail@eliu.de>
2021-02-01 20:12:57 +0100

    landingpage permission required

--- elios_app/views.py
+++ elios_app/views.py
@@ -1,7 +1,9 @@
 from django.template.response import TemplateResponse
 from django.http import HttpResponseNotAllowed
+from django.contrib.auth.decorators import permission_required
 
 
+@permission_required('core.landingpage_right')
 def test_view(request):
     context = dict()
     context['data'] = f"You are logged in as {request.user} ({request.user.username})."
