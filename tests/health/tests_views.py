dfa610e6efeccf3bd8efe885c36de99902edf673
elius <mail@eliu.de>
2021-02-07 14:26:03 +0100

    test for health views

--- tests/health/tests_views.py
+++ tests/health/tests_views.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
 from django.template.response import TemplateResponse
 from django.shortcuts import reverse
@@ -11,3 +13,31 @@ class WeightListTest(CreateUserMixin, TestCase):
         response = self.client.get(reverse("health-weight"))
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response, TemplateResponse)
+
+    def test_get_response_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse("health-weight"))
+        self.assertEqual(response.status_code, 302)
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+    def test_with_data(self):
+        data = {
+            'created_by': self.user,
+            'timestamp_created': timezone.now(),
+            'timestamp_changed': timezone.now(),
+            'weight': 80.8,
+            'measurement_date': datetime.date(2020, 1, 1)
+        }
+        Weight.objects.create(**data)
+        response = self.client.get(reverse("health-weight"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+        self.assertEqual(response.context_data['object_list'].count(), 1)
+        self.client.logout()
+        self.client.login(username="bdam", password="123456")
+        response = self.client.get(reverse("health-weight"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, TemplateResponse)
+        self.assertEqual(response.context_data['object_list'].count(), 0)
+
+
--- tests/texts_mixins.py
+++ tests/texts_mixins.py
@@ -6,11 +6,19 @@ from core.models import User, PermissionRegister
 
 class CreateUserMixin:
     def setUp(self):
-        perm = Permission.objects.get(content_type=ContentType.objects.get_for_model(PermissionRegister))
-        group = Group.objects.create(**{'name': "StandardUsers"})
-        group.permissions.add(perm)
+        group_standard = Group.objects.create(**{'name': "StandardUsers"})
+        group_standard.permissions.add(Permission.objects.get(
+            content_type=ContentType.objects.get_for_model(PermissionRegister),
+            codename="landingpage_right"))
+        group_health = Group.objects.create(**{'name': "HealthApp"})
+        group_health.permissions.add(Permission.objects.get(
+            content_type=ContentType.objects.get_for_model(PermissionRegister),
+            codename="health_app"))
         self.client = Client()
         self.user = User.objects.create_user("adam", "adam@adam.de", "123456")
-        group.user_set.add(self.user)
         self.user2 = User.objects.create_user("bdam", "adam@adam.de", "123456")
+        group_standard.user_set.add(self.user)
+        group_standard.user_set.add(self.user2)
+        group_health.user_set.add(self.user)
+        group_health.user_set.add(self.user2)
         self.client.login(username="adam", password="123456")
