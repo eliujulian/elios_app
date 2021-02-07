3bf42ad29cd6bdf8c6af15724859cab688fb1cec
elius <mail@eliu.de>
2021-02-07 14:23:58 +0100

    reorganized urls for better overview

--- elios_app/urls.py
+++ elios_app/urls.py
@@ -22,22 +22,37 @@ from core.views import *
 from health.views import *
 
 
-urlpatterns = [
+basic_urls = [
     path("", login_required(test_view, login_url="login-url"), name="landingpage"),
     path("success/", MessageView.as_view(), name="message-success-public"),
     path("failure/", MessageView.as_view(), name="message-failure-public"),
     path("login/", LoginView.as_view(template_name="application/login.html"), name="login-url"),
     path("logout/", LogoutView.as_view(template_name="application/login.html"), name="logout-url"),
-    path("register/", AccountRegisterView.as_view(), name="account-register"),
     path("user/<slug>/", login_required(UserDetailView.as_view()), name="user-detail"),
+]
+
+account_management_urls = [
+    path("register/", AccountRegisterView.as_view(), name="account-register"),
     path("account/confirm/", AccountConfirmEMailView.as_view(), name="account-confirm"),
     path("account/<slug>/", login_required(AccountDetailView.as_view()), name="account-detail"),
     path("account/<slug>/update/", login_required(AccountUpdateView.as_view()), name="account-update"),
-    path("account/<slug>/delete/", login_required(AccountDeleteView.as_view()), name="account-delete"),
-    path("admin/", admin.site.urls),
+    path("account/<slug>/delete/", login_required(AccountDeleteView.as_view()), name="account-delete")
+]
+
+admin_urls = [
+    path("admin/", admin.site.urls)
+]
+
+health_urls = [
     path("health/weight/", login_required(WeightListView.as_view()), name="health-weight"),
     path("health/weight/create/", login_required(WeightCreateView.as_view()), name="health-weight-create"),
     path("health/weight/<int:pk>/", login_required(WeightDetailView.as_view()), name="health-weight-detail"),
     path("health/weight/<int:pk>/update/", login_required(WeightDetailView.as_view()), name="health-weight-update"),
-
+    path("health/weight/<int:pk>/delete/", login_required(WeightDeleteView.as_view()), name="health-weight-delete"),
 ]
+
+urlpatterns = list()
+urlpatterns.extend(basic_urls)
+urlpatterns.extend(account_management_urls)
+urlpatterns.extend(admin_urls)
+urlpatterns.extend(health_urls)
