28c37ef3bcd6d39bd9f2d453aca70ea467edf904
elius <mail@eliu.de>
2021-02-15 10:06:27 +0100

    knowledge models implementation

--- knowledge/models.py
+++ knowledge/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from core.models import AbstractBaseModel
 
-# Create your models here.
+
+class Book(AbstractBaseModel):
+    class Meta:
+        abstract = True
+
+    title = models.CharField(max_length=160)
+    author = models.CharField(max_length=160)
+    year = models.IntegerField()
+    is_favorite = models.BooleanField(default=False)
+    summary = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.author} - {self.title} ({str(self.year)})'
+
+
+class Chapter(AbstractBaseModel):
+    class Meta:
+        abstract = True
+
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    title = models.CharField(max_length=160)
+    summary = models.TextField(null=True, blank=True)
+    order_num = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
