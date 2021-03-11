b351bb424b7277413835ac25a640b273742ddd3f
elius <mail@eliu.de>
2021-03-11 17:38:49 +0100

    grid habit list changed update afer ajay call added

--- static/js/eliosapp.js
+++ static/js/eliosapp.js
@@ -1,8 +1,7 @@
 
-function post_call(url, data, callback, callnow) {
+function post_call(url, data, callback) {
     let csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value
 
-    console.log(url, data, csrf, callback, callnow);
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
@@ -19,17 +18,35 @@ function post_call(url, data, callback, callnow) {
         params += "&"
     }
 
-    console.log(params);
     xhr.open('POST', url, true);
     xhr.setRequestHeader("X-CSRFToken", csrf);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("Content-length", params.length);
     xhr.setRequestHeader("Connection", "close");
     xhr.send(params);
-    console.log("end of post_call()");
 }
 
 
 function update_habit(data) {
-    console.log(data);
+    let el = document.getElementById(data['id_slug']);
+    let streak = document.getElementById(data['id_slug'] + "-current_streak");
+    let new_streak = data['current_streak'];
+    streak.innerHTML = "Reihe: " + new_streak.toString();
+    btns = el.getElementsByClassName("btn");
+    var i;
+    for (i = 0; i < btns.length; i++) {
+        btns[i].disabled = true;
+    }
+    if (data['last_day_action'] === 1) {
+        btns[0].classList.remove("btn-outline-primary");
+        btns[0].classList.add("btn-primary");
+    }
+    if (data['last_day_action'] === 2) {
+        btns[1].classList.remove("btn-outline-primary");
+        btns[1].classList.add("btn-primary");
+    }
+    if (data['last_day_action'] === 3) {
+        btns[2].classList.remove("btn-outline-primary");
+        btns[2].classList.add("btn-primary");
+    }
 }
